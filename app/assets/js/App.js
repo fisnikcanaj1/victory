318d55fda27366e1a2dacbeb3f72f805a380b699
Your Name <fisnikcanaj2@gmail.com>
2018-07-01 15:38:21 +0200

    Form validation done

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,10 +1,11 @@
 // import RevealOnScroll from './modules/RevealOnScroll';
 import Validator from './modules/Validator';
 import SwapContent from './modules/SwapContent';
-// import $ from 'jquery'; 
+import $ from 'jquery'; 
 import 'bootstrap';
 import 'popper.js';
 import 'jquery-validation';
+import { log } from 'util';
 
 // var mobileMenu = new MobileMenu();
 // new RevealOnScroll($(".feature-item"), '85%');
@@ -12,3 +13,36 @@ import 'jquery-validation';
 // new StickyHeader();
 new Validator();
 new SwapContent();
+
+$("#login-id").validate({
+    rules: {
+        username: {
+            required: true,
+            // Using the normalizer to trim the value of the element
+            // before validating it.
+            //
+            // The value of `this` inside the `normalizer` is the corresponding
+            // DOMElement. In this example, `this` references the `username` element.
+            normalizer: function (value) {
+                return $.trim(value);
+            }
+        }
+    }
+});
+
+$("#register-id").validate({
+    rules: {
+        username: {
+            required: true,
+            // Using the normalizer to trim the value of the element
+            // before validating it.
+            //
+            // The value of `this` inside the `normalizer` is the corresponding
+            // DOMElement. In this example, `this` references the `username` element.
+            normalizer: function (value) {
+                return $.trim(value);
+            }
+        }
+    }
+});
+
--- app/assets/js/modules/Validator.js
+++ app/assets/js/modules/Validator.js
@@ -3,25 +3,27 @@ import $ from 'jquery';
 
 class Validator {
     constructor() {
+        this.form = $('form');
         this.inputs = $('.login-inputs');
         this.validationMessage = $('.validation-message');
         this.validator();
     }
 
     validator () {
-        this.inputs.on('change', function () {
-            let emailValue = $(this).val();
-            // let passwodValue = $()
-            let emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-            console.log(emailValue);
+        // this.inputs.on('change', function (e) {
+        //     e.preventDefault();
+        //     let emailValue = $(this).val();
+        //     // let passwodValue = $()
+        //     let emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        //     console.log(emailValue);
             
-            if (emailValue == '') {
-                $('.validation-message').html(`<p class='alert-message'>Type your email and password!</p>`);
-            }
-            else {
-                $('.validation-message').html('');
-            }
-        });
+        //     if (emailValue == '') {
+        //         $('.validation-message').html(`<p class='alert-message'>Type your email and password!</p>`);
+        //     }
+        //     else {
+        //         $('.validation-message').html('');
+        //     }
+        // });
     }
 }
 
